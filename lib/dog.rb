c2120d32f0080b6302e6245fbfa2b410d90b6cf5
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-29 16:45:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Dog 
+  def initialize(name, bark)
+    @name = name 
+    @bark = bark 
+  end 
+  # get dog name
+  def name 
+    @name
+  end 
+  # get dog bark 
+  def bark
+    puts "#{@bark}"
+  end
+  # set dog name 
+  def name=(value)
+    @name=value 
+  end 
+  #set dog bark 
+  def bark=(value)
+    @bark=value 
+  end 
+  
+end 
